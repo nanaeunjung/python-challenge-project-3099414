875dc3983e4033ce56cb9d7e1f62218a56ee50a1
nanaeunjung <142706223+nanaeunjung@users.noreply.github.com>
2023-09-14 14:22:42 +0900

    Create 00_03_Laying_out_the_code.py

---
+++ 00_03_Laying_out_the_code.py
@@ -0,0 +1,14 @@
+import time
+
+for i in range(0,20):
+  print(' '*i + '.')
+  time.sleep(0.1)
+
+import os
+def clear():
+  os.system('cls' if os.name=='nt' else 'clear')
+for i in range(0,20):
+  print('\n\n\n')
+  print(' '*i + '.')
+  time.sleep(0.1)
+  clear()
